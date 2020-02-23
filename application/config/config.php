4f662733466f53d308698389cd87a2bbacb68b55
ranab5b <rana.bilal@meritincentives.com>
2020-02-23 18:53:09 +0500

    Admin Dashboard - Config

--- application/config/config.php
+++ application/config/config.php
@@ -23,10 +23,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+
+//$config['base_url'] = 'http://localhost/sadizameen/';
 $config['base_url'] = 'http://sadizameen.com/';
 
 define('ASSETS_URL', $config['base_url'] . 'assets/') ; 		
-
+define('ADMIN_ASSETS_URL', $config['base_url'] . 'assets/admin/') ;
 
 
 
